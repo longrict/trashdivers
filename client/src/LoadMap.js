0331c08d2d9828e6967300a23f564621ef9f22bf
Phouman <ryanphou@live.com>
2024-08-03 22:27:10 -0400

    MAP THAT WORKS

--- client/src/LoadMap.js
+++ client/src/LoadMap.js
@@ -1,29 +1,79 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useState, useEffect, useCallback } from 'react';
+import {APIProvider,Map,useMap, AdvancedMarker,Pin} from "@vis.gl/react-google-maps"
+;
+const PoiMarkers = props => {
+  const map = useMap()
+  const [markers, setMarkers] = useState({})
 
-const containerStyle = {
-  width: '100%',
-  height: '400px'
-};
+  const handleClick = useCallback(ev => {
+    if (!map) return
+    if (!ev.latLng) return
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+    //console.log('marker clicked: ', ev.latLng.toString());
+    map.panTo(ev.latLng)
+  })
+
+  console.log(props)
+  console.log(props.pois)
+
+  return (
+    <>
+      {props.pois.map(poi => (
+        <AdvancedMarker
+          key={poi.key}
+          position={poi.location}
+          onClick={handleClick}
+        >
+          <Pin
+            background={"#FBBC04"}
+            glyphColor={"#000"}
+            borderColor={"#000"}
+          />
+        </AdvancedMarker>
+      ))}
+    </>
+  )
+}
+
 
 function LoadMap() {
-  return (
-    <LoadScript googleMapsApiKey={process.env.REACT_GOOGLE_MAP_API_KEY}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+  const [locations, setLocations] = useState([])
 
-  );
+  const onMapClick = (ev) => {
+    setLocations([...locations, 
+      { 
+      key: ev.detail.latLng.lat.toString(),
+      location: ev.detail.latLng
+    }])
+  }
+  
+  return (
+    <div style={{height:"90vh"}}>
+      <APIProvider
+      
+        apiKey= {process.env.REACT_APP_GOOGLE_MAP_API_KEY}
+        onLoad={() => console.log("Maps API has loaded.")}
+      >
+        <Map
+          defaultZoom={13}
+          defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+          onCameraChanged= {ev =>
+            console.log(
+              "camera changed:",
+              ev.detail.center,
+              "zoom:",
+              ev.detail.zoom
+            )
+          }
+          mapId="da37f3254c6a6d1c"
+          onClick={onMapClick}
+        >
+          <PoiMarkers pois={locations} />
+        </Map>
+      </APIProvider>
+
+    </div>
+  )
 }
 
 export {LoadMap};
