cd55bc5d1912faf56e48e15a9d1df64f90c159e6
longrict <longrictran@gmail.com>
2024-08-03 22:29:35 -0400

    landing page

--- client/src/header.js
+++ client/src/header.js
@@ -3,7 +3,10 @@ import './header.css';
 function Header() {
     return (
         <div className="banner">
-            <h1 className="bannerName">TrashDivers</h1>
+            <h1 className="bannerName">
+                <img src="/images/tree-2-svgrepo-com.svg" style={{height:'30px', width:'auto'}}></img>
+                TrashDivers
+            </h1>
             <div>
                 <ul className="links">
                     <li><a>Home</a></li>
